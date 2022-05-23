22efd7eb937e87472ee7b96c07e6ddfdc075b521
Peach <p22635501@gmail.com>
2022-05-23 18:58:19 +0800

    新增頁面切換鈕 css調整

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 let token=document.getElementById('btn1');
 let connect=document.getElementById('btn2');
+let block=document.querySelector('.block');
 let head=document.querySelector('.heading');
 let sec2=document.querySelector('.field2').style;
 let sec3=document.querySelector('.field3').style;
 let sec4=document.querySelector('.field4').style;
+let pageList=document.querySelector('.change-page').style;
 
 window.onload=goDifferentPage("login")
 
 function common(){
+    head.innerHTML="Token Claim Dashboard"
     connect.innerHTML="Disconnect Wallet"
     token.style.display="inline-block"
     sec2.display="inline-block"
     sec3.display="inline-block"
     sec4.display="none"
+    pageList.top="15%"
+    token.style.width="60%"
+    connect.style.width="60%"
+    block.style.width="80%"
+    head.style.margin="0"
+    head.style.padding="0"
+    //RWD
+    if(window.innerWidth<=768){
+        block.style.margin="5vh"
+        block.style.padding="20px"
+        pageList.top="5%"
+    }else if(window.innerWidth<=1024){
+        block.style.height="90vh"
+        block.style.margin="5vh"
+        pageList.top="10%"
+    }
 }
 
 function goDifferentPage(page){
@@ -25,29 +44,24 @@ function goDifferentPage(page){
             break;
         case "sec1":
             common()
+            token.innerHTML="NFT Holder Claim Tokens"
             break;
         case "sec2":
-            token.innerHTML="MOD Claim Tokens"
             common()
+            token.innerHTML="MOD Claim Tokens"
             break;
         case "sec3":
-            token.innerHTML="DAO Foundation Claim Tokens"
             common()
+            token.innerHTML="DAO Foundation Claim Tokens"
             break;
         case "sec4":
+            common()
             connect.innerHTML="Disconnect Wallet"
             token.innerHTML="Claim Tokens"
             head.innerHTML="AirDrop"
-            token.style.display="inline-block"
             sec2.display="none"
             sec3.display="none"
             sec4.display="inline-block"
             break;          
-    }
-}
-//條件判斷要去的頁面
-function judgePage(){
-    if(true){
-        goDifferentPage("sec1")
     }
 }
