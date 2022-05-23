89180861d4a6431745432d6c7cb08404d8d0ac8b
DavidColor <davidcolor153@gmail.com>
2022-05-24 01:08:21 +0800

    05.24 RWD

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ let head=document.querySelector('.heading');
 let sec2=document.querySelector('.field2').style;
 let sec3=document.querySelector('.field3').style;
 let sec4=document.querySelector('.field4').style;
-let pageList=document.querySelector('.change-page').style;
 
 window.onload=goDifferentPage("login")
 
@@ -16,23 +15,18 @@ function common(){
     sec2.display="inline-block"
     sec3.display="inline-block"
     sec4.display="none"
-    pageList.top="15%"
-    token.style.width="60%"
-    connect.style.width="60%"
+    token.style.width="50%"
+    connect.style.width="50%"
     block.style.width="80%"
-    head.style.margin="0"
-    head.style.padding="0"
-    //RWD
-    if(window.innerWidth<=768){
-        block.style.margin="5vh"
-        block.style.padding="20px"
-        pageList.top="5%"
-    }else if(window.innerWidth<=1024){
-        block.style.height="90vh"
-        block.style.margin="5vh"
-        pageList.top="10%"
+    if(window.innerWidth<=900){
+        token.style.width="100%"
+        connect.style.width="100%"
+    }
+    if(window.innerWidth<=426){
+        block.style.width="90vw"
     }
 }
+
 
 function goDifferentPage(page){
     switch(page){
